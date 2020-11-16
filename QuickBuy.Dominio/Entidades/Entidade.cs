c35317319e4a19b1cc29a04ca43298da18149451
TIAGO ALMEIDA <tiagoalmeida3000@gmail.com>
2020-11-16 11:55:07 -0300

    Especificando Contratos do Repositorio

--- QuickBuy.Dominio/Entidades/Entidade.cs
+++ QuickBuy.Dominio/Entidades/Entidade.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace QuickBuy.Dominio.Entidades
 {
     public abstract class Entidade
     {
+        public List<string> _mensagensValidacao { get; set; }
+        private List<string> mensagensValidacao 
+        { 
+            get { return _mensagensValidacao ?? (_mensagensValidacao = new List<string>()); }     
+        }
 
+        protected void LimparMensagensValidacao()
+        {
+            mensagensValidacao.Clear();
+        }
+
+        protected void AdicionarMensagem(string mensagem)
+        {
+            mensagensValidacao.Add(mensagem);
+        }
+
+        public abstract void Validate();
+        protected bool EhValido
+        {
+            get {return !mensagensValidacao.Any(); }
+        }
     }
 }
--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,11 +1,12 @@
 ﻿using QuickBuy.Dominio.ObjetoDeValor;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Pedido
+    public class Pedido : Entidade
     {
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
@@ -19,5 +20,16 @@ namespace QuickBuy.Dominio.Entidades
         public int FormaPagamentoId { get; set; }
         public FormaPagamento FormaPagamento { get; set; }
         public ICollection<ItemPedido> ItensPedido { get; set; }
+
+        public override void Validate()
+        {
+            LimparMensagensValidacao();
+
+            if (!ItensPedido.Any())
+                AdicionarMensagem("Item de Pedido não pode ficar vazio!");
+
+            if(string.IsNullOrEmpty(CEP))
+                AdicionarMensagem("CEP não pode ficar vazio!");
+        }
     }
 }
